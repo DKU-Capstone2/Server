bcb46a63f054754a4a9ab1a97517c4175dc0efca
pawoo0211 <pawoo0211@naver.com>
2022-09-11 14:31:54 +0900

    initial commit

---
+++ src/main/java/com/capstone2/dku/DkuApplication.java
@@ -0,0 +1,12 @@
+package com.capstone2.dku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkuApplication.class, args);
+	}
+}

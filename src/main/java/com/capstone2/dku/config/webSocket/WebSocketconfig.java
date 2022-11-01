1116b41647f7535c980d6e0d3e73bfc0aabe9198
ChunEunyu <82995817+ChunEunyu@users.noreply.github.com>
2022-11-01 19:44:50 +0900

    웹소켓 만들고있습니다.

---
+++ src/main/java/com/capstone2/dku/config/webSocket/WebSocketconfig.java
@@ -0,0 +1,19 @@
+package com.capstone2.dku.config.webSocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+// @EnableWebSocket
+
+/**
+public class WebSocketconfig implements WebSocketConfigurer {
+    private final WebSocketHandler webSocketHandler;
+
+    @override
+    public void registerWebSocketHandlers(WebSocketHandler) {
+        return webSocketHandler;
+    }
+}
+ **/

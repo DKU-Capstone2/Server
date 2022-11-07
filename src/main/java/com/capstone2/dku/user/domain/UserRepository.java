cc2c55e68555350ab6a101e8d53db61b0346e963
pawoo0211 <pawoo0211@naver.com>
2022-11-07 20:34:10 +0900

    feat: 타입별 작가 리스트 조회
    작가 엔티티에 타입 필드 추가

--- src/main/java/com/capstone2/dku/user/domain/UserRepository.java
+++ src/main/java/com/capstone2/dku/user/domain/UserRepository.java
@@ -1,8 +1,14 @@
 package com.capstone2.dku.user.domain;
 
+import com.capstone2.dku.writer.domain.Writer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     User findByEmail(String email);
+
     Boolean existsByEmail(String email);
+
+    List<Writer> findAllByType(String type);
 }
